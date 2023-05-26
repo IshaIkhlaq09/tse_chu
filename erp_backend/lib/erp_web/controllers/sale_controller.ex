bdfdcaa0358ebbf6e5a722b7712ebbd5458cb4c9
hamnaashraf10455 <128869958+hamnaashraf10455@users.noreply.github.com>
2023-05-26 10:08:17 +0500

    Update sale_controller.ex

--- erp_backend/lib/erp_web/controllers/sale_controller.ex
+++ erp_backend/lib/erp_web/controllers/sale_controller.ex
@@ -7,13 +7,19 @@ A module that acts as the controller for system sales.
     @doc """
   Process a singular sale of a bike.
   """
-  def process_sale(conn,  %{"sale" => sale_params}) do
-    user = Guardian.Plug.current_resource(conn)
-    case Erp.Sale.add_sale(sale_params, user) do
-      {:ok} ->
-        json(conn, %{success: ":)"})
-      {:error, error} ->
-        {:error, error}
-    end
+def process_sale(conn, %{"sale" => sale_params}) do
+  # Retrieve the current user from the connection using Guardian authentication
+  user = Guardian.Plug.current_resource(conn)
+
+  # Call the add_sale function from the Erp.Sale module, passing sale_params and user
+  case Erp.Sale.add_sale(sale_params, user) do
+    {:ok} ->
+      # If the sale was successfully added, send a JSON response with success message
+      json(conn, %{success: ":)"})
+
+    {:error, error} ->
+      # If an error occurred during the sale addition, return the error tuple
+      {:error, error}
   end
 end
+
