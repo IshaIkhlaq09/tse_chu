2c7e612f719d2c9077a17d5a4b244eccf63d7bd5
EmaanAjmal99 <88826181+EmaanAjmal99@users.noreply.github.com>
2023-05-26 11:32:53 +0500

    Update material_controller.ex

--- erp_backend/lib/erp_web/controllers/material_controller.ex
+++ erp_backend/lib/erp_web/controllers/material_controller.ex
@@ -2,18 +2,19 @@ defmodule ErpWeb.MaterialController do
 @moduledoc """
 A module that acts as the controller for managing materials.
 """
-  use ErpWeb, :controller //The code defines a controller module ErpWeb.MaterialController responsible for managing materials. 
+  use ErpWeb, :controller 
+  #The code defines a controller module ErpWeb.MaterialController responsible for managing materials. 
   import Ecto.Query, warn: false
 
   alias Erp.Production.Material
-//The show function retrieves and renders a single material based on the provided material ID.
+#The show function retrieves and renders a single material based on the provided material ID.
   @doc false
   def show(conn, %{"id" => material_id}) do
     material = Erp.Production.Material.get_material!(material_id)
     render(conn, "show.json", material: material)
   end
 
-The show_all_materials/2 function retrieves all materials and renders them in JSON format.
+#The show_all_materials function retrieves all materials and renders them in JSON format.
   @doc """
   Show all materials.
   """
@@ -21,7 +22,7 @@ The show_all_materials/2 function retrieves all materials and renders them in JS
     materials = Erp.Production.Material.list_materials()
     render(conn, "index.json", materials: materials)
   end
-//The get_materials_by_plant_id function retrieves a list of materials to a specific plant ID and renders them in JSON format.
+#The get_materials_by_plant_id function retrieves a list of materials to a specific plant ID and renders them in JSON format.
   @doc """
   Get a list of materials that correlate to a plant ID.
   """
