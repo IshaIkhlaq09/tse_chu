43419dc09bd21d5450eae3cdbdaeea3b2cc31208
IshaIkhlaq09 <126484394+IshaIkhlaq09@users.noreply.github.com>
2023-05-26 10:31:14 +0500

    Update order_controller.ex

--- erp_backend/lib/erp_web/controllers/order_controller.ex
+++ erp_backend/lib/erp_web/controllers/order_controller.ex
@@ -6,7 +6,14 @@ A module that acts as the controller for managing orders (customer transactions)
   import Ecto.Query, warn: false
   import Guardian.Plug
 
-  @doc false
+  @doc """ false 
+  This function handles the display of details for a specific order.
+  It takes a connection (conn) and a map containing the order ID (%{"id" => id}).
+  The order ID is parsed and converted to an integer using Integer.parse/1. 
+  The corresponding order is retrieved using Erp.Sales.Order.get_order!/1,
+  and the ! indicates that an exception will be raised if the order with the given ID is not found.
+  """
+  
   def show(conn, %{"id" => id}) do
     {int, _rem} = Integer.parse(id)
     order = Erp.Sales.Order.get_order!(int)
@@ -20,9 +27,12 @@ A module that acts as the controller for managing orders (customer transactions)
 
   @doc """
   Show a list of all orders.
+  this function captures the process of retrieving and displaying all orders,
+  and responding with the list of orders in JSON format.
+  
   """
-  def show_all_orders(conn, _params) do
-    orders = Erp.Sales.Order.list_orders()
-    render(conn, "index.json", orders: orders)
-  end
+ # Display all orders
+def show_all_orders(conn, _params) do
+  orders = Erp.Sales.Order.list_orders()  # Retrieve the list of all orders
+  render(conn, "index.json", orders: orders)  # Render the "index.json" template with the list of orders
 end
